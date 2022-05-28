f954c9c98b6c88bfce61b3d70391aa73666c3306
Frashier <75584735+Frashier@users.noreply.github.com>
2022-05-28 18:40:13 +0200

    text refactoring, homepage content and responsiveness

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -1,51 +1,48 @@
 import "./Homepage.css";
 import "../../index.css";
 import CustomDiv from "./CustomDiv";
+import content from "../../translations/polish.json";
 import { motion } from "framer-motion";
 
 function Homepage() {
   return (
     <motion.div className="homepage">
-      <CustomDiv className="homepage_header"></CustomDiv>
+      <CustomDiv className="homepage_header">
+        <h1>{content.homepage.mainHeader}</h1>
+      </CustomDiv>
       <CustomDiv className="homepage_body" style={{ gridArea: "first-text" }}>
-        <h1>Blockchain</h1>
-        <p>
-          Blockchain to jednokierunkowa lista rekordów nazywanych blokami. Bloki
-          można dodawać jedynie na końcu listy, a każdy z nich jest pozwiązany z
-          poprzednikiem przy pomocy funkcji haszującej.
-        </p>
+        <h1>{content.homepage.header[0]}</h1>
+        <p>{content.homepage.body[0]}</p>
       </CustomDiv>
       <CustomDiv className="homepage_body" style={{ gridArea: "first-image" }}>
         <img
           src="Assets/Blockchain.svg"
           alt="Blockchain structure"
-          style={{ float: "right", height: "500px", gridArea: "first-image" }}
+          style={{ height: "500px" }}
         ></img>
       </CustomDiv>
       <CustomDiv className="homepage_body" style={{ gridArea: "second-text" }}>
-        <h1>Blok</h1>
-        <p>
-          Blok to podstawowy element składowy blockchain. Składa się z nagłówka
-          zawierającego hasz zawartości poprzedniego bloku, korzeń drzewa haszy
-          i nonce oraz danych zawierających zapis transakcji wykonanych w danym
-          bloku.
-        </p>
+        <h1>{content.homepage.header[1]}</h1>
+        <p>{content.homepage.body[1]}</p>
       </CustomDiv>
       <CustomDiv className="homepage_body" style={{ gridArea: "second-image" }}>
         <img
           src="Assets/Bitcoin_Block_data.svg"
           alt="Blockchain structure"
-          style={{ float: "left", maxWidth: "100%" }}
+          style={{ maxWidth: "100%" }}
         ></img>
       </CustomDiv>
-      <CustomDiv
-        className="homepage_body"
-        style={{ gridArea: "third-text" }}
-      ></CustomDiv>
-      <CustomDiv
-        className="homepage_body"
-        style={{ gridArea: "thid-image" }}
-      ></CustomDiv>
+      <CustomDiv className="homepage_body" style={{ gridArea: "third-text" }}>
+        <h1>{content.homepage.header[2]}</h1>
+        <p>{content.homepage.body[2]}</p>
+      </CustomDiv>
+      <CustomDiv className="homepage_body" style={{ gridArea: "third-image" }}>
+        <img
+          src="Assets/P2P_network.svg"
+          alt="Blockchain newtork"
+          style={{ maxWidth: "100%" }}
+        ></img>
+      </CustomDiv>
     </motion.div>
   );
 }
